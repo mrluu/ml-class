5880597cea8c4770811bf7ec5d3f15bfc3c2d71d
Tieu Luu <tieu@qmulos.com>
2018-03-03 14:27:40 -0500

    implemented findClosestCentroids and computeCentroids

---
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,39 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% Number of examples;
+m = size(X, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+
+dist = zeros(m,K);
+
+for i = 1:K
+  centroid = centroids(i,:);
+  D = bsxfun(@minus, X, centroid);
+  dist(:,i) = sum(D.^2,2);
+end
+
+[minDist, idx] = min(dist, [ ], 2);
+
+
+% =============================================================
+
+end
+
